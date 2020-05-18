034b86c3ca5f4e8e97b6367c96dc2b2bc6943664
PonnapaliKausik7 <65541824+PonnapaliKausik7@users.noreply.github.com>
2020-05-18 16:45:23 +0530

    Create database.js

---
+++ util/database.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host : 'localhost',
+    user : 'root',
+    database: 'node',
+    password: 'Qwerty1234'
+
+    
+});
+
+module.exports = pool.promise();
+
