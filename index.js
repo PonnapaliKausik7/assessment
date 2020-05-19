10222bf1a656fd89f198735773f6d6088c52c7ff
PonnapaliKausik7 <65541824+PonnapaliKausik7@users.noreply.github.com>
2020-05-19 20:51:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const db = require('./util/database');
 const {  notFound , handleError } = require('./util/error-handler')
 //Getting sequelize object
 const sequelize = require('./util/sequelize');
+//logging
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({name: 'assessment' , streams: [
+    {
+      level: 'info',
+      stream: process.stdout            // log INFO and above to stdout
+    },
+    {
+      level: 'info',
+      path: './info.log'  // log info and above to a file
+    }
+  ]});
+
 
 //For auto creating models from tables in Data base
 const sequelizeAuto = require('./util/sequelizeAuto');
@@ -13,17 +26,17 @@ const app = new express()
 
 const router = require('./routers/mainRouter.js')
 
-//Routers
 router.mainRouter(app)
-
-//exception handling
 app.use(notFound)
 app.use(handleError)
+
+log.info('welcome');
+//log.warn({lang: 'english'}, 'au revoir');
 
 const service = {
     async run() {
         
-        await mongo.connect(env.MONGO_CONNECTION_STRING, env.DB_NAME);
+        //await mongo.connect(env.MONGO_CONNECTION_STRING, env.DB_NAME);
         await app.listen(env.PORT);
         
         //The below command should be run only if there are any new tables in SQL data bases. 
@@ -38,6 +51,9 @@ const service = {
 };
 
 service.run().catch((err) => {console.log("error starting server", err)
+
+
+
 process.exit(1);
 });
 
